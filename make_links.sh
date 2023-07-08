5003701b6aca7cc990d6cfda2fda23cb9161a51a
Benjamin Mayes <czahrien@gmail.com>
2023-07-08 17:13:06 -0400

    Rewrote script to use GNU Stow. Greatly simplifies the problem this is trying to solve.

---
+++ make_links.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+
+BASENAME=$(basename ${0})
+BASEDIR=$(readlink -f $(dirname ${0}))
+DEST_PREFIX=${DEST_PREFIX-~}
+
+PLATFORM_PREFIX=$(uname -s -m | tr ' ' '-')
+PLATFORM_PREFIX=${PLATFORM_PREFIX:l}
+
+HOST_PREFIX="${HOST-}"
+
+# check_git - Check git repository for updates and pull latest copy of code from origin
+check_git() {
+  pushd $BASEDIR
+  if git remote show origin > /dev/null 2>&1; then
+    git pull origin main
+  fi
+  popd
+}
+
+
+make_links() {
+  PKG_DIR=${BASEDIR}/dotfiles
+  PLATFORM_DIR=${BASEDIR}/platform_dotfiles/${PLATFORM_PREFIX}
+  HOST_DIR=${BASEDIR}/host_dotfiles/${HOST_PREFIX}
+
+  mkdir -p ${PLATFORM_DIR}/zsh/
+  mkdir -p ${HOST_DIR}/zsh/
+
+  for x in $PKG_DIR $PLATFORM_DIR $HOST_DIR; do
+    pushd $x
+    stow --verbose --restow --target="${HOME}" * 2>&1
+    popd
+  done
+}
+
+main() {
+  check_git
+  make_links
+}
+
+main
